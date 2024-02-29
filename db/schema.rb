09979c816b48043acb16bf0cf8108337744c4168
Otkr519 <romans519otkr@gmail.com>
2024-03-01 00:10:59 +0900

    add:ストロングパラメーターにnameを許可

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2024_02_28_205115) do
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
